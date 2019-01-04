e82b298e793b5e589a2a1b58bb238f0a303d2699
waldonemilio <waldonemilio@gmail.com>
2019-01-04 19:22:09 +0000

    Remove useless grep after copied images have been hidden

--- i3-wpp.sh
+++ i3-wpp.sh
@@ -3,7 +3,7 @@
 path2wallpappers=$1
 i=0
 
-ls $path2wallpappers | sort -R | grep -v img | head | 
+ls $path2wallpappers | sort -R | head | 
 	while read file; 
 	do 
 		cp $path2wallpappers/$file $path2wallpappers/.img$i; 
